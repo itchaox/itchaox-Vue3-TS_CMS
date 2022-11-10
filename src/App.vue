4942c57dd9c2c7b5bd1f69bba0051a0ec8b90f51
王超 <wangchao2423@gmail.com>
2022-11-10 16:31:20 +0800

    feat(Element-Plus): 集成到项目中

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,31 @@
  * @Author: wc
  * @Date: 2022-11-08 14:06:51
  * @LastEditors: wc
- * @LastEditTime: 2022-11-10 14:21:04
+ * @LastEditTime: 2022-11-10 16:30:29
 -->
 <template>
-  <div class="App">
-    <h1>App</h1>
+  <div class="app">
+    <h1>app</h1>
     <router-link to="/login">login</router-link><br />
     <router-link to="/main">main</router-link>
     <router-view></router-view>
+    <el-row class="mb-4">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
 <style lang="less" scoped>
-.App {
-  color: red;
+.app {
+  height: 100vh;
+  width: 100vw;
+  background-color: cornsilk;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,23 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// 局部注册 Element-Plus
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
+
   resolve: {
     alias: {
       // 代码路径打包后提示
