b2792b31c7cbe34dd29a1ae109bf5e1cf5da29c6
itchaox <1462831573@qq.com>
2022-11-17 23:11:04 +0800

    ✨ feat(store/login): 动态路由基本思路实现

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,11 @@
+/*
+ * @Desc:
+ * @Version: v1.00
+ * @Author: wangchao
+ * @Date: 2022-11-10 21:21:17
+ * @LastEditors: wangchao
+ * @LastEditTime: 2022-11-17 22:18:29
+ */
 /// <reference types="vite/client" />
 
 // 声明 vue 文件
--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -3,8 +3,8 @@
  * @Version: v1.00
  * @Author: wc
  * @Date: 2022-11-14 11:02:08
- * @LastEditors: wc
- * @LastEditTime: 2022-11-16 15:03:01
+ * @LastEditors: wangchao
+ * @LastEditTime: 2022-11-17 23:05:02
  */
 
 import { defineStore } from 'pinia'
@@ -18,6 +18,7 @@ import type { IAccount } from '@/types'
 import { localCache } from '@/utils/cache'
 import router from '@/router'
 import { TOKEN } from '@/global/constants'
+import type { RouteRecordRaw } from 'vue-router'
 
 interface ILoginStore {
   token: string
@@ -50,6 +51,28 @@ const useLoginStore = defineStore('login', {
       const userMenusData = await getUserMenusByRoleId(this.userInfo.role.id)
       this.userMenus = userMenusData.data
       localCache.setCache('userMenus', userMenusData.data)
+
+      /**
+       * ! 动态路由思路
+       * 1. 获取菜单
+       * 2. 将路由对象放入一个路由数组中
+       * 3. 根据菜单去匹配路由数组中，应该添加那些路由，到实际加载路由中
+       */
+
+      const localRouters: RouteRecordRaw[] = [] // 路由数组
+      // 2.1 读取 router/main 中所有 ts 文件
+      const files: Record<string, any> = import.meta.glob(
+        '../../router/main/**/*.ts',
+        {
+          eager: true
+        }
+      ) // eager:true 立即加载文件
+
+      for (const key in files) {
+        const module = files[key] // 获取模块
+        localRouters.push(module.default) // 向路由数组中添加路由对象
+      }
+      console.log('localRouters', localRouters)
 
       // 5. 跳转至 main 页面
       router.push('/main')
