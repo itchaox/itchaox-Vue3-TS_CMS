ea6aaa304087e13fbc6def84469b3747bb741fb5
王超 <wangchao2423@gmail.com>
2022-11-24 13:38:13 +0800

    feat(user): dialog 对话框公共组件

--- src/components/content/main-dialog/main-dialog.vue
+++ src/components/content/main-dialog/main-dialog.vue
@@ -4,13 +4,73 @@
  * @Author: wc
  * @Date: 2022-11-23 11:23:35
  * @LastEditors: wc
- * @LastEditTime: 2022-11-23 16:35:44
+ * @LastEditTime: 2022-11-24 13:33:39
 -->
 
 <template>
   <div class="main-dialog">
-    <el-dialog v-model="isShowDialog" title="Warning" width="30%" center>
-      <span>Open the dialog from the center from the screen</span>
+    <el-dialog v-model="isShowDialog" title="新建角色" width="30%" center>
+      <div class="form">
+        <el-form
+          size="large"
+          :model="formData"
+          label-width="100px"
+          :rules="dialogFormRules"
+        >
+          <el-form-item label="用户名" prop="name">
+            <el-input v-model="formData.name" placeholder="请输入用户名" />
+          </el-form-item>
+          <el-form-item label="真实姓名" prop="realname">
+            <el-input
+              v-model="formData.realname"
+              placeholder="请输入真实姓名"
+            />
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input
+              v-model="formData.password"
+              placeholder="请输入密码"
+              show-password
+            />
+          </el-form-item>
+          <el-form-item label="手机号码" prop="cellphone">
+            <el-input
+              v-model="formData.cellphone"
+              placeholder="请输入手机号码"
+            />
+          </el-form-item>
+          <el-form-item label="选择角色" prop="roleId">
+            <el-select
+              v-model="formData.roleId"
+              placeholder="请选择角色"
+              size="large"
+              style="width: 100%"
+            >
+              <el-option
+                v-for="item in [{ value: '前端开发', roleId: 3 }]"
+                :key="item.value"
+                :label="item.value"
+                :value="item.roleId"
+              />
+            </el-select>
+          </el-form-item>
+          <el-form-item label="选择部门" prop="departmentId">
+            <el-select
+              v-model="formData.departmentId"
+              placeholder="请选择部门"
+              size="large"
+              style="width: 100%"
+            >
+              <el-option
+                v-for="item in [{ value: '研发五部', roleId: 2 }]"
+                :key="item.value"
+                :label="item.value"
+                :value="item.roleId"
+              />
+            </el-select>
+          </el-form-item>
+        </el-form>
+      </div>
       <template #footer>
         <span class="dialog-footer">
           <el-button @click="isShowDialog = false">取消</el-button>
@@ -24,9 +84,64 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import type { FormRules } from 'element-plus'
+import { reactive, ref } from 'vue'
 
-const isShowDialog = ref(false) // 对话框显隐
+const isShowDialog = ref(true) // 对话框显隐
+const formData = reactive({
+  name: '',
+  realname: '',
+  password: '',
+  cellphone: '',
+  roleId: '',
+  departmentId: ''
+})
+
+const dialogFormRules = reactive<FormRules>({
+  name: [
+    {
+      required: true,
+      message: '请输入用户名',
+      trigger: 'blur'
+    }
+  ],
+
+  realname: [
+    {
+      required: true,
+      message: '请输入真实姓名',
+      trigger: 'blur'
+    }
+  ],
+  password: [
+    {
+      required: true,
+      message: '请输入密码',
+      trigger: 'blur'
+    }
+  ],
+  cellphone: [
+    {
+      required: true,
+      message: '请输入手机号码',
+      trigger: 'blur'
+    }
+  ],
+  roleId: [
+    {
+      required: true,
+      message: '请选择角色',
+      trigger: 'blur'
+    }
+  ],
+  departmentId: [
+    {
+      required: true,
+      message: '请选择部门',
+      trigger: 'blur'
+    }
+  ]
+})
 
 // 暴露属性
 defineExpose({ isShowDialog })
