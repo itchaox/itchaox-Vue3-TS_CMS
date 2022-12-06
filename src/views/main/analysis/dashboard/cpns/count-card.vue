55c60e5f334830964b706637306966b975dcc724
王超 <wangchao2423@gmail.com>
2022-12-06 17:30:37 +0800

    feat(count-card.vue): 新增countup.js实现数字动画效果

--- src/views/main/analysis/dashboard/cpns/count-card.vue
+++ src/views/main/analysis/dashboard/cpns/count-card.vue
@@ -4,24 +4,49 @@
  * @Author: wc
  * @Date: 2022-12-06 14:32:05
  * @LastEditors: wc
- * @LastEditTime: 2022-12-06 16:53:07
+ * @LastEditTime: 2022-12-06 17:28:38
 -->
 
 <template>
   <div class="count-card">
     <div class="title">{{ title }}</div>
-    <div class="count">{{ number1 }}</div>
-    <div>{{ title }} {{ number1 }}</div>
+    <div class="count" ref="number1Ref">{{ number1 }}</div>
+    <div>
+      <span>
+        {{ title }}
+      </span>
+      <span ref="number2Ref">
+        {{ number1 }}
+      </span>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { CountUp } from 'countup.js'
+import { onMounted, ref } from 'vue'
+
 interface IProps {
+  amount: string
   title: string
   number1: number
 }
 
-defineProps<IProps>()
+const props = defineProps<IProps>()
+
+const number1Ref = ref<HTMLElement>()
+const number2Ref = ref<HTMLElement>()
+
+const countOption = {
+  prefix: props.amount === 'saleroom' ? '¥' : ''
+}
+onMounted(() => {
+  const countup1 = new CountUp(number1Ref.value!, props.number1, countOption)
+  const countup2 = new CountUp(number2Ref.value!, props.number1, countOption)
+
+  countup1.start()
+  countup2.start()
+})
 </script>
 
 <style lang="less" scoped>
