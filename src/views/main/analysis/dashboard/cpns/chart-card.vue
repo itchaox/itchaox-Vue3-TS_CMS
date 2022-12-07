d2aaf7097abcd55244a318c79f740aae3ddb9c15
王超 <wangchao2423@gmail.com>
2022-12-07 13:28:20 +0800

    feat(chart-card.vue): 封装 Echarts 表格组件

---
+++ src/views/main/analysis/dashboard/cpns/chart-card.vue
@@ -0,0 +1,31 @@
+<!--
+ * @desc: 图表卡片
+ * @Version: v1.00
+ * @Author: wc
+ * @Date: 2022-12-07 10:41:47
+ * @LastEditors: wc
+ * @LastEditTime: 2022-12-07 13:16:28
+-->
+
+<template>
+  <div class="chart-card">
+    <el-card class="box-card" :header="header ?? '销量展示'">
+      <slot></slot>
+    </el-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+interface IProps {
+  header: string
+}
+defineProps<IProps>()
+</script>
+
+<style lang="less" scoped>
+.chart-card {
+  color: red;
+}
+</style>
--- src/views/main/analysis/dashboard/dashboard.vue
+++ src/views/main/analysis/dashboard/dashboard.vue
@@ -4,10 +4,11 @@
  * @Author: wc
  * @Date: 2022-11-17 17:06:44
  * @LastEditors: wc
- * @LastEditTime: 2022-12-06 16:52:31
+ * @LastEditTime: 2022-12-07 13:21:37
 -->
 <template>
   <div class="dashboard">
+    <!-- 1. header -->
     <el-row :gutter="12">
       <template v-for="item in amountList" :key="item.title">
         <el-col :span="6">
@@ -15,11 +16,37 @@
         </el-col>
       </template>
     </el-row>
+
+    <!-- 2. content -->
+    <el-row :gutter="12">
+      <el-col :span="7">
+        <chart-card header="分类商品数量（饼图）">2</chart-card>
+      </el-col>
+
+      <el-col :span="10">
+        <chart-card header="不同城市商品销量">3</chart-card>
+      </el-col>
+
+      <el-col :span="7">
+        <chart-card header="分类商品数量（玫瑰图）">5</chart-card>
+      </el-col>
+    </el-row>
+
+    <!-- 3. footer -->
+    <el-row :gutter="10">
+      <el-col :span="12">
+        <chart-card header="分类商品销量">22</chart-card>
+      </el-col>
+      <el-col :span="12">
+        <chart-card header="分类商品收藏">23</chart-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script setup lang="ts" name="dashboard">
 import CountCard from './cpns/count-card.vue'
+import ChartCard from './cpns/chart-card.vue'
 
 import useAnalysisStore from '@/store/main/analysis/analysis'
 import { storeToRefs } from 'pinia'
@@ -32,4 +59,8 @@ analysisStore.getWholeAnalysisData()
 const { amountList } = storeToRefs(analysisStore) // storeToRefs 映射出响应式数据
 </script>
 
-<style scoped></style>
+<style scoped>
+.el-row {
+  margin-bottom: 15px;
+}
+</style>
