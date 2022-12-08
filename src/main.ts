ccce2abc41f00dc97a150350a37bd9f6d7778490
王超 <wangchao2423@gmail.com>
2022-12-08 16:01:30 +0800

    style(main.ts): 修复代码格式

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@
  * @Author: wc
  * @Date: 2022-11-08 14:06:51
  * @LastEditors: wc
- * @LastEditTime: 2022-11-23 17:16:34
+ * @LastEditTime: 2022-12-08 16:00:11
  */
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -18,9 +18,5 @@ import icons from './global/register-icons'
 const app = createApp(App)
 app.use(icons) // 注册 element-plus icon
 app.use(store) // 注册状态
-
-
-
-
 app.use(router) // 注册路由
 app.mount('#app') // 挂载页面到 app
